8e380391d1a9d09fa521021d4fb7f1752182a123
Alessandro Tegner <alessandro.tegner@gmail.com>
2016-04-02 07:39:39 -0700

    First commit

---
+++ Gemfile
@@ -0,0 +1,37 @@
+source 'https://rubygems.org'
+
+gem 'rails', '>= 5.0.0.beta3', '< 5.1'
+gem 'pg', '~> 0.18'
+gem 'puma'
+
+# Assets stuffs
+gem 'sass-rails', '~> 5.0'
+gem 'uglifier', '>= 1.3.0'
+gem 'therubyracer', platforms: :ruby
+
+# Frontend stuffs
+gem 'jquery-rails'
+gem 'turbolinks', '~> 5.x'
+
+group :development, :test do
+  gem 'byebug'
+  gem 'rspec-rails', '>= 3.5.0.beta2'
+end
+
+group :development do
+  gem 'web-console', '~> 3.0'
+  gem 'listen', '~> 3.0.5'
+  gem 'spring'
+  gem 'spring-watcher-listen', '~> 2.0.0'
+
+  gem 'rubocop', require: false
+  gem 'overcommit', require: false
+end
+
+group :test do
+  gem 'shoulda-matchers', '>= 3.1.1'
+  gem 'factory_girl', '>= 4.5.0'
+  gem 'database_cleaner', '>= 1.5.1'
+  gem 'capybara', '>= 2.6.2'
+  gem 'simplecov', require: false
+end
---
+++ spec/rails_helper.rb
@@ -0,0 +1,36 @@
+# This file is copied to spec/ when you run 'rails generate rspec:install'
+ENV['RAILS_ENV'] ||= 'test'
+require File.expand_path('../../config/environment', __FILE__)
+
+# Prevent database truncation if the environment is production
+if Rails.env.production?
+  abort('The Rails environment is running in production mode!')
+end
+
+if ENV['COV'].present?
+  require 'simplecov'
+  SimpleCov.start 'rails'
+end
+
+require 'spec_helper'
+require 'rspec/rails'
+ActiveRecord::Migration.maintain_test_schema!
+
+RSpec.configure do |config|
+  config.fixture_path = "#{::Rails.root}/spec/fixtures"
+  config.use_transactional_fixtures = true
+  config.infer_spec_type_from_file_location!
+  config.filter_rails_from_backtrace!
+
+  config.include FactoryGirl::Syntax::Methods
+  config.before(:suite) do
+    FactoryGirl.reload
+
+    begin
+      DatabaseCleaner.start
+      FactoryGirl.lint
+    ensure
+      DatabaseCleaner.clean
+    end
+  end
+end
